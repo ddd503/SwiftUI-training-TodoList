47e5636175a152e0678edd9b1e94f790b6ac380f
Dai Kawahara <skbsporta7@gmail.com>
2021-09-18 11:11:06 +0900

    add create command

--- SwiftUI-training-TodoList/SwiftUI-training-TodoList/Infrastructure/DataStore/TodoDataStore.swift
+++ SwiftUI-training-TodoList/SwiftUI-training-TodoList/Infrastructure/DataStore/TodoDataStore.swift
@@ -6,18 +6,33 @@
 //
 
 import Combine
+import Foundation
 
 protocol TodoDataStore {
-    func create(title: String, content: String?) -> AnyPublisher<[Todo], Never>
+    func create(title: String, content: String?) -> AnyPublisher<Void, Error>
     func read() -> AnyPublisher<[Todo], Never>
     func update(todo: Todo) -> AnyPublisher<[Todo], Never>
     func delete(todo: Todo) -> AnyPublisher<[Todo], Never>
 }
 
 struct TodoDataStoreImpl: TodoDataStore {
-    func create(title: String, content: String?) -> AnyPublisher<[Todo], Never> {
-        // 仮で値を返す
-        Just([Todo]()).eraseToAnyPublisher()
+    func create(title: String, content: String?) -> AnyPublisher<Void, Error> {
+        let context = CoreDataManager.shared.container.viewContext
+        let newTodo = Todo(context: context)
+        // TODO: 最大値に1足した値がidになる
+        //        newTodo.id = ??
+        newTodo.title = title
+        newTodo.content = content
+        newTodo.editDate = Date()
+        return Future<Void, Error> { promise in
+            do {
+                try context.save()
+                promise(.success(()))
+            } catch {
+                promise(.failure(error))
+            }
+        }
+        .eraseToAnyPublisher()
     }
 
     func read() -> AnyPublisher<[Todo], Never> {

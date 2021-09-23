0e41d80b4dc79ca780260165993789d5e0a97300
Dai Kawahara <skbsporta7@gmail.com>
2021-09-23 14:48:45 +0900

    add update and find command

--- SwiftUI-training-TodoList/SwiftUI-training-TodoList/Infrastructure/DataStore/TodoDataStore.swift
+++ SwiftUI-training-TodoList/SwiftUI-training-TodoList/Infrastructure/DataStore/TodoDataStore.swift
@@ -12,7 +12,7 @@ import CoreData
 protocol TodoDataStore {
     func create(title: String, content: String?) -> AnyPublisher<Void, Error>
     func read() -> AnyPublisher<[Todo], Error>
-    func update(todo: Todo) -> AnyPublisher<[Todo], Never>
+    func update(todo: Todo) -> AnyPublisher<[Todo], Error>
     func delete(todo: Todo) -> AnyPublisher<[Todo], Never>
 }
 
@@ -25,15 +25,7 @@ struct TodoDataStoreImpl: TodoDataStore {
         newTodo.title = title
         newTodo.content = content
         newTodo.editDate = Date()
-        return Future<Void, Error> { promise in
-            do {
-                try context.save()
-                promise(.success(()))
-            } catch {
-                promise(.failure(error))
-            }
-        }
-        .eraseToAnyPublisher()
+        return save(context: context)
     }
 
     func read() -> AnyPublisher<[Todo], Error> {
@@ -41,24 +33,80 @@ struct TodoDataStoreImpl: TodoDataStore {
         let fetchRequest = NSFetchRequest<Todo>(entityName: "Todo")
         let sortDescriptor = NSSortDescriptor(key: "id", ascending: true)
         fetchRequest.sortDescriptors = [sortDescriptor]
-        return Future<[Todo], Error> { promise in
-            do {
-                let todoList = try context.fetch(fetchRequest)
-                promise(.success(todoList))
-            } catch {
-                promise(.failure(error))
+        return Deferred {
+            Future<[Todo], Error> { promise in
+                do {
+                    let todoList = try context.fetch(fetchRequest)
+                    promise(.success(todoList))
+                } catch {
+                    promise(.failure(error))
+                }
             }
         }
         .eraseToAnyPublisher()
     }
 
-    func update(todo: Todo) -> AnyPublisher<[Todo], Never> {
-        // 仮で値を返す
-        Just([Todo]()).eraseToAnyPublisher()
+    func update(todo: Todo) -> AnyPublisher<[Todo], Error> {
+        let context = CoreDataManager.shared.container.viewContext
+        return find(id: todo.id, context: context)
+            .flatMap { foundTodo -> AnyPublisher<Void, Error> in
+                foundTodo?.title = todo.title
+                foundTodo?.content = todo.content
+                foundTodo?.editDate = Date()
+                return save(context: context)
+            }
+            .flatMap {_ -> AnyPublisher<[Todo], Error> in
+                read()
+            }
+            .eraseToAnyPublisher()
     }
 
     func delete(todo: Todo) -> AnyPublisher<[Todo], Never> {
         // 仮で値を返す
         Just([Todo]()).eraseToAnyPublisher()
+    }
+
+    private func find(id: Int64, context: NSManagedObjectContext) -> AnyPublisher<Todo?, Error> {
+        let fetchRequest = NSFetchRequest<Todo>(entityName: "Todo")
+        let predicate = NSPredicate(format: "id == %@", id)
+        fetchRequest.predicate = predicate
+        let sortDescriptor = NSSortDescriptor(key: "id", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        return Deferred {
+            Future<Todo?, Error> { promise in
+                do {
+                    let fetchResult = try context.fetch(fetchRequest)
+                    // 検索結果なし
+                    guard !fetchResult.isEmpty else {
+                        promise(.success(nil))
+                        return
+                    }
+                    // idが重複している
+                    guard fetchResult.count == 1 else {
+                        promise(.success(nil))
+                        return
+                    }
+
+                    promise(.success(fetchResult.first))
+                } catch {
+                    promise(.failure(error))
+                }
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+
+    private func save(context: NSManagedObjectContext) -> AnyPublisher<Void, Error> {
+        Deferred {
+            Future<Void, Error> { promise in
+                do {
+                    try context.save()
+                    promise(.success(()))
+                } catch {
+                    promise(.failure(error))
+                }
+            }
+        }
+        .eraseToAnyPublisher()
     }
 }

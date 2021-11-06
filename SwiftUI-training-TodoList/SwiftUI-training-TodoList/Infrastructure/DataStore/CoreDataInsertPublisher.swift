b57781b76bc40efaae3b57754d764bff8c50e1a9
Dai Kawahara <skbsporta7@gmail.com>
2021-11-06 10:52:54 +0900

    fix CoreDataInsertPublisher

--- SwiftUI-training-TodoList/SwiftUI-training-TodoList/Infrastructure/DataStore/CoreDataInsertPublisher.swift
+++ SwiftUI-training-TodoList/SwiftUI-training-TodoList/Infrastructure/DataStore/CoreDataInsertPublisher.swift
@@ -19,8 +19,49 @@ struct CoreDataInsertPublisher: Publisher {
     let editDate: Date
 
     func receive<S>(subscriber: S) where S : Subscriber, Failure == S.Failure, Output == S.Input {
-        // ここでアクションを実行して結果をsubscriberに返す
-        // アクションの実行に必要な要素は初期化時にDIする
+        // subscriberの登録
+        // 購読時に実行する処理を登録
+        let subscription = CoreDataInsertPublisher.Subscription(subscriber: subscriber,
+                                                                context: context,
+                                                                uuid: uuid,
+                                                                title: title,
+                                                                content: content,
+                                                                editDate: editDate)
+        subscriber.receive(subscription: subscription)
+    }
+}
+
+extension CoreDataInsertPublisher {
+    class Subscription<S> where S : Subscriber, Failure == S.Failure, Output == S.Input {
+        // Sを使うためにextensionでインナークラスにしている
+        private var subscriber: S?
+        let context: NSManagedObjectContext
+        let uuid: String
+        let title: String
+        let content: String?
+        let editDate: Date
+
+        init(subscriber: S,
+             context: NSManagedObjectContext,
+             uuid: String,
+             title: String,
+             content: String?, editDate: Date) {
+            self.subscriber = subscriber
+            self.context = context
+            self.uuid = uuid
+            self.title = title
+            self.content = content
+            self.editDate = editDate
+        }
+    }
+}
+
+extension CoreDataInsertPublisher.Subscription: Subscription {
+    func request(_ demand: Subscribers.Demand) {
+        // 購読時毎に実行される処理
+        var demand = demand
+        guard let subscriber = subscriber, demand > 0 else { return }
+
         let newTodo = Todo(context: context)
         newTodo.uuid = uuid
         newTodo.title = title
@@ -28,22 +69,17 @@ struct CoreDataInsertPublisher: Publisher {
         newTodo.editDate = editDate
 
         do {
+            demand -= 1
             try context.save()
-            _ = subscriber.receive(newTodo)
-            subscriber.receive(completion: .finished)
+            demand += subscriber.receive(newTodo)
+//            subscriber.receive(completion: .finished)
         } catch {
             subscriber.receive(completion: .failure(error))
         }
-
-        // 購読完了の通知
-        subscriber.receive(subscription: CoreDataSaveSubscription(combineIdentifier: CombineIdentifier()))
     }
-}
 
-struct CoreDataInsertSubscription: Subscription {
-    let combineIdentifier: CombineIdentifier
-
-    func request(_ demand: Subscribers.Demand) {}
-
-    func cancel() {}
+    func cancel() {
+        // 購読キャンセル時はsubscriberをnilにして購読登録なくす
+        subscriber = nil
+    }
 }

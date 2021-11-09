a3231f31fb951c07ee508088f88765839315b7e1
Dai Kawahara <skbsporta7@gmail.com>
2021-11-09 22:15:24 +0900

    fix CoreDataFetchPublisher

--- SwiftUI-training-TodoList/SwiftUI-training-TodoList/Infrastructure/DataStore/CoreDataFetchPublisher.swift
+++ SwiftUI-training-TodoList/SwiftUI-training-TodoList/Infrastructure/DataStore/CoreDataFetchPublisher.swift
@@ -16,22 +16,45 @@ struct CoreDataFetchPublisher<DataModel>: Publisher where DataModel: NSManagedOb
     let request: NSFetchRequest<DataModel>
     
     func receive<S>(subscriber: S) where S : Subscriber, Failure == S.Failure, Output == S.Input {
-        do {
-            let result = try context.fetch(request)
-            _ = subscriber.receive(result)
-            subscriber.receive(completion: .finished)
-        } catch {
-            subscriber.receive(completion: .failure(error))
-        }
-        
-        subscriber.receive(subscription: CoreDataFetchSubscription(combineIdentifier: CombineIdentifier()))
+        let subscription =
+        CoreDataFetchPublisher.Subscription(subscriber: subscriber,
+                                            context: context,
+                                            request: request)
+        subscriber.receive(subscription: subscription)
     }
 }
 
-struct CoreDataFetchSubscription: Subscription {
-    let combineIdentifier: CombineIdentifier
+extension CoreDataFetchPublisher {
+    class Subscription<S> where S : Subscriber, Failure == S.Failure, Output == S.Input {
+        // Sを使うためにextensionでインナークラスにしている
+        private var subscriber: S?
+        let context: NSManagedObjectContext
+        let request: NSFetchRequest<DataModel>
 
-    func request(_ demand: Subscribers.Demand) {}
+        init(subscriber: S,
+             context: NSManagedObjectContext,
+             request: NSFetchRequest<DataModel>) {
+            self.subscriber = subscriber
+            self.context = context
+            self.request = request
+        }
+    }
+}
+
+extension CoreDataFetchPublisher.Subscription: Subscription {
+    func request(_ demand: Subscribers.Demand) {
+        var demand = demand
+        guard let subscriber = subscriber, demand > 0 else { return }
+
+        do {
+            demand -= 1
+            let result = try context.fetch(request)
+            demand += subscriber.receive(result)
+//            subscriber.receive(completion: .finished)
+        } catch {
+            subscriber.receive(completion: .failure(error))
+        }
+    }
 
     func cancel() {}
 }
